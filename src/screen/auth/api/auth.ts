9505456166e7767a5ca330c985e6bc192058b647
cho4u4o <cho4u4o@gmail.com>
2025-06-23 01:11:41 +0900

    refactor : change method

--- src/screen/auth/api/auth.ts
+++ src/screen/auth/api/auth.ts
@@ -1,10 +1,6 @@
-import { useMutation } from '@tanstack/react-query';
-import { useSetAtom } from 'jotai';
+import { useQuery } from '@tanstack/react-query';
 
-import { post, REQUEST } from '@/shared/api';
-import { RAW_PATH } from '@/shared/constants';
-import { userTokenAtom } from '@/shared/atom';
-import { replace } from '@/shared/utils';
+import { get, REQUEST } from '@/shared/api';
 
 interface KakaoLoginRequest {
   code: string;
@@ -22,32 +18,16 @@ interface KakaoLoginResponse {
 }
 
 const submitKakaoLogin = async (code: string) => {
-  const response = await post<KakaoLoginRequest, KakaoLoginResponse>({
+  const response = await get<KakaoLoginResponse, KakaoLoginRequest>({
     request: REQUEST.LOGIN,
-    data: { code: code },
+    params: { code: code },
   });
   return response.data;
 };
 
-export const useKakaoLogin = () => {
-  const setKakaoLogin = useSetAtom(userTokenAtom);
-
-  return useMutation<KakaoLoginResponse, Error, { code: string }>({
-    mutationFn: ({ code }) => submitKakaoLogin(code),
-    onSuccess: data => {
-      const kakaoAccessToken = data.tokenDto.accessToken;
-      setKakaoLogin({
-        accessToken: kakaoAccessToken,
-      });
-      if (data.signedUp) replace(RAW_PATH.HOME);
-      else {
-        alert('회원가입 화면으로 이동합니다!');
-        replace(RAW_PATH.SIGNUP);
-      }
-    },
-    onError: () => {
-      alert('로그인에 실패했어요. 다시 시도해 주세요!');
-      replace(RAW_PATH.HOME);
-    },
+export const useKakaoLogin = (code: string) => {
+  return useQuery<KakaoLoginResponse>({
+    queryKey: ['kakaoLogin', code],
+    queryFn: () => submitKakaoLogin(code),
   });
 };
--- src/screen/auth/ui/AuthScreen.tsx
+++ src/screen/auth/ui/AuthScreen.tsx
@@ -1,20 +1,53 @@
 import { useEffect } from 'react';
+import { useSetAtom } from 'jotai';
 import { useKakaoLogin } from '@/screen/auth/api';
 import { LoginBg } from '@/assets/image';
 
-export default function AuthScreen() {
-  const params = new URLSearchParams(location.search);
-  const code = params.get('code');
+import { replace } from '@/shared/utils';
+import { RAW_PATH } from '@/shared/constants';
+import { userTokenAtom } from '@/shared/atom';
 
-  const { mutate, isPending, isError } = useKakaoLogin();
+export default function AuthScreen() {
+  const setUserToken = useSetAtom(userTokenAtom);
+  const params = new URLSearchParams(location.search);
+
+  const getCode = () => {
+    const code = params.get('code');
+    if (!code) {
+      alert('로그인에 실패했어요. 다시 시도해 주세요!');
+      replace(RAW_PATH.HOME);
+      return '';
+    }
+    return code;
+  };
+
+  const { data, isError, isFetching } = useKakaoLogin(getCode());
 
   useEffect(() => {
-    if (code) mutate({ code });
-  }, [code, mutate]);
+    if (data) {
+      const kakaoAccessToken = data.tokenDto.accessToken;
+      setUserToken({
+        accessToken: kakaoAccessToken,
+      });
+      if (data.signedUp) {
+        replace(RAW_PATH.HOME);
+
+        if (data.signedUp) replace(RAW_PATH.HOME);
+      } else {
+        alert('회원가입 화면으로 이동합니다!');
+
+        replace(RAW_PATH.SIGNUP);
+      }
+    }
+    if (isError) {
+      alert('로그인에 실패했어요. 다시 시도해 주세요!');
+      replace(RAW_PATH.HOME);
+    }
+  }, [data, isError, setUserToken]);
 
   return (
     <div className="container-mobile grid h-screen place-items-center">
-      {isPending && <div className="flex flex-col"></div>}
+      {isFetching && <div className="flex flex-col" />}
       {isError && (
         <div className="flex w-full flex-col items-center justify-center gap-2 text-lg">
           <img src={LoginBg} />
