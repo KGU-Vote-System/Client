f21c37ef931517173e40c579b0de5eb899994d69
cho4u4o <cho4u4o@gmail.com>
2025-06-22 18:35:06 +0900

    feat : implement user type

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,3 +1,4 @@
 export * from './path';
 export * from './bottomSheet';
 export * from './vote';
+export * from './user';
---
+++ src/shared/types/user.ts
@@ -0,0 +1,9 @@
+export type User = {
+  kakaoEmail: string;
+  name: string;
+  collegeMajorName: string;
+  studentEmail: string;
+  walletAddress: string;
+  keyId: string;
+  krn: string;
+};
