2a1e45df537fea7489c5a414f06dd12a821c9bfd
cho4u4o <cho4u4o@gmail.com>
2025-06-23 19:12:08 +0900

    fix : change method of kakao login into `GET`

--- src/screen/auth/api/auth.ts
+++ src/screen/auth/api/auth.ts
@@ -1,10 +1,58 @@
-import { useMutation } from '@tanstack/react-query';
+// import { useMutation } from '@tanstack/react-query';
 
-import { post, REQUEST } from '@/shared/api';
-import { RAW_PATH } from '@/shared/constants';
-import { replace } from '@/shared/utils';
-import { userTokenAtom } from '@/shared/atom';
-import { useSetAtom } from 'jotai';
+// import { post, REQUEST } from '@/shared/api';
+// import { RAW_PATH } from '@/shared/constants';
+// import { replace } from '@/shared/utils';
+// import { userTokenAtom } from '@/shared/atom';
+// import { useSetAtom } from 'jotai';
+
+// interface KakaoLoginRequest {
+//   code: string;
+// }
+
+// interface KakaoLoginResponse {
+//   kakaoEmail: string;
+//   tokenDto: {
+//     grantType: string;
+//     accessToken: string;
+//     accessTokenExpiresIn: number;
+//     refreshToken: string;
+//   };
+//   signedUp: boolean;
+// }
+
+// const submitKakaoLogin = async (code: string) => {
+//   const response = await post<KakaoLoginRequest, KakaoLoginResponse>({
+//     request: `${REQUEST.LOGIN}?code=${code}`,
+//   });
+//   return response.data;
+// };
+
+// export const useKakaoLogin = () => {
+//   const setKakaoLogin = useSetAtom(userTokenAtom);
+//   return useMutation<KakaoLoginResponse, Error, { code: string }>({
+//     mutationFn: ({ code }) => submitKakaoLogin(code),
+//     onSuccess: data => {
+//       const kakaoAccessToken = data.tokenDto.accessToken;
+//       setKakaoLogin({
+//         accessToken: kakaoAccessToken,
+//       });
+//       if (data.signedUp) replace(RAW_PATH.HOME);
+//       else {
+//         alert('회원가입 화면으로 이동합니다!');
+//         replace(RAW_PATH.SIGNUP);
+//       }
+//     },
+//     onError: () => {
+//       alert('로그인에 실패했어요. 다시 시도해 주세요!');
+//       replace(RAW_PATH.HOME);
+//     },
+//   });
+// };
+
+import { useQuery } from '@tanstack/react-query';
+
+import { get, REQUEST } from '@/shared/api';
 
 interface KakaoLoginRequest {
   code: string;
@@ -22,30 +70,16 @@ interface KakaoLoginResponse {
 }
 
 const submitKakaoLogin = async (code: string) => {
-  const response = await post<KakaoLoginRequest, KakaoLoginResponse>({
-    request: `${REQUEST.LOGIN}?code=${code}`,
+  const response = await get<KakaoLoginResponse, KakaoLoginRequest>({
+    request: REQUEST.LOGIN,
+    params: { code: code },
   });
   return response.data;
 };
 
-export const useKakaoLogin = () => {
-  const setKakaoLogin = useSetAtom(userTokenAtom);
-  return useMutation<KakaoLoginResponse, Error, { code: string }>({
-    mutationFn: ({ code }) => submitKakaoLogin(code),
-    onSuccess: data => {
-      const kakaoAccessToken = data.tokenDto.accessToken;
-      setKakaoLogin({
-        accessToken: kakaoAccessToken,
-      });
-      if (data.signedUp) replace(RAW_PATH.HOME);
-      else {
-        alert('회원가입 화면으로 이동합니다!');
-        replace(RAW_PATH.SIGNUP);
-      }
-    },
-    onError: () => {
-      alert('로그인에 실패했어요. 다시 시도해 주세요!');
-      replace(RAW_PATH.HOME);
-    },
+export const useKakaoLogin = (code: string) => {
+  return useQuery<KakaoLoginResponse>({
+    queryKey: ['kakaoLogin', code],
+    queryFn: () => submitKakaoLogin(code),
   });
 };
--- src/screen/auth/ui/AuthScreen.tsx
+++ src/screen/auth/ui/AuthScreen.tsx
@@ -1,18 +1,62 @@
+// import { useEffect } from 'react';
+// import { useKakaoLogin } from '@/screen/auth/api';
+// import { LoginBg } from '@/assets/image';
+
+// import Loader from '@/shared/ui/Loader';
+
+// export default function AuthScreen() {
+//   const params = new URLSearchParams(location.search);
+//   const code = params.get('code');
+
+//   const { mutate } = useKakaoLogin();
+
+//   useEffect(() => {
+//     if (code) mutate({ code });
+//   }, [code, mutate]);
+
 import { useEffect } from 'react';
+import { useSetAtom } from 'jotai';
 import { useKakaoLogin } from '@/screen/auth/api';
 import { LoginBg } from '@/assets/image';
 
+import { replace } from '@/shared/utils';
+import { RAW_PATH } from '@/shared/constants';
+import { userTokenAtom } from '@/shared/atom';
 import Loader from '@/shared/ui/Loader';
 
 export default function AuthScreen() {
+  const setUserToken = useSetAtom(userTokenAtom);
   const params = new URLSearchParams(location.search);
-  const code = params.get('code');
 
-  const { mutate } = useKakaoLogin();
+  const getCode = () => {
+    const code = params.get('code');
+    if (!code) {
+      alert('로그인에 실패했어요. 다시 시도해 주세요!');
+      replace(RAW_PATH.HOME);
+      return '';
+    }
+    return code;
+  };
+
+  const { data, isError } = useKakaoLogin(getCode());
 
   useEffect(() => {
-    if (code) mutate({ code });
-  }, [code, mutate]);
+    if (data) {
+      const kakaoAccessToken = data.tokenDto.accessToken;
+      setUserToken({
+        accessToken: kakaoAccessToken,
+      });
+      if (data.signedUp) replace(RAW_PATH.HOME);
+      else {
+        alert('회원가입 화면으로 이동합니다!');
+        replace(RAW_PATH.SIGNUP);
+      }
+    }
+    if (isError) {
+      alert('로그인에 실패했어요. 다시 시도해 주세요!');
+      replace(RAW_PATH.HOME);
+    }
+  }, [data, isError, setUserToken]);
 
   return (
     <div className="container-mobile relative grid h-screen place-items-center overflow-hidden">
