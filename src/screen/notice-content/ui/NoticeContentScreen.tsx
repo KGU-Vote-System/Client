29ead0922cb5f42161e555f791fe6efeebb8ef64
cho4u4o <cho4u4o@gmail.com>
2025-06-24 18:16:49 +0900

    refactor : change parameter type of `NoticeContentScreen`

--- src/screen/notice-content/ui/NoticeContentScreen.tsx
+++ src/screen/notice-content/ui/NoticeContentScreen.tsx
@@ -5,11 +5,9 @@ import type { Notice } from '@/features/notice/types';
 import type { ActivityComponentType } from '@stackflow/react';
 import { NoticeContentContainer } from '@/widgets/notice-content/ui';
 
-const NoticeContentScreen: ActivityComponentType<{ notice: Notice }> = ({
-  params,
-}: {
-  params: { notice: Notice };
-}) => {
+const NoticeContentScreen: ActivityComponentType<{
+  notice: Omit<Notice, 'noticeType'>;
+}> = ({ params }: { params: { notice: Omit<Notice, 'noticeType'> } }) => {
   const { title, id, startAt, endAt } = params.notice;
   return (
     <AppScreen backgroundColor="#fff" appBar={TitleAppBar('공지사항')}>
