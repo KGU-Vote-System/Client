8428872f685c0754e606d927195d4fcd86aebe2f
cho4u4o <cho4u4o@gmail.com>
2025-06-25 02:00:20 +0900

    feat : send data to UI pieces

--- src/widgets/vote-promise/ui/VoteCandidate.tsx
+++ src/widgets/vote-promise/ui/VoteCandidate.tsx
@@ -1,13 +1,20 @@
-interface VoteCandidateProps {
-  name: string;
-}
-export default function VoteCandidate({ name }: VoteCandidateProps) {
+import type { Nominee } from '@/shared/types';
+
+export default function VoteCandidate({
+  name,
+  main,
+  college,
+  department,
+  studentId,
+  description,
+}: Nominee) {
   return (
-    <>
-      <p className="mb-1.5 text-2xl font-bold">정후보자 {name}</p>
-      <p className="mb-2.5 font-normal">컴퓨터공학부 04</p>
-      <p className="text-s text-sm">기아기아 최강기아 역임</p>
-      <p className="text-s text-sm">기아의 해결사 역임</p>
-    </>
+    <div className="flex w-full flex-col">
+      <p className="mb-1.5 text-2xl font-bold">
+        {main ? '정후보자' : '부후보자'} {name}
+      </p>
+      <p className="mb-2.5 font-normal">{`${college} ${department} ${studentId.slice(2, 4)}`}</p>
+      <p className="text-s text-sm">{description}</p>
+    </div>
   );
 }
--- src/widgets/vote-promise/ui/VotePromiseCard.tsx
+++ src/widgets/vote-promise/ui/VotePromiseCard.tsx
@@ -2,15 +2,60 @@ import { useState } from 'react';
 import { motion } from 'framer-motion';
 
 import { VerifiedCheckIcon } from '@/assets/icon';
-import VoteCandidate from './VoteCandidate';
+import type { Nominee } from '@/shared/types';
 import { cn } from '@/shared/utils';
 
-export default function VotePromiseCard() {
+import VoteCandidate from './VoteCandidate';
+
+interface VotePromiseCardProps {
+  nominees: Nominee[];
+  candidateName: string;
+}
+
+export default function VotePromiseCard({
+  nominees,
+  candidateName,
+}: VotePromiseCardProps) {
   const [flipped, setFlipped] = useState(false);
+
+  const CardFront = () => (
+    <div className="border-m shadow-voteItem absolute flex h-full w-full flex-col rounded-lg border-[1px] bg-white p-6 backface-hidden">
+      <div className="mb-5 flex w-full justify-between">
+        <TeamBadge team={candidateName} />
+        <span className="text-m font-normal">1/2</span>
+      </div>
+      <div className="flex w-full flex-col gap-y-[34px]">
+        {nominees.map(nominee => (
+          <VoteCandidate key={nominee.id} {...nominee} />
+        ))}
+      </div>
+    </div>
+  );
+
+  const CardBack = () => (
+    <div className="border-m shadow-voteItem bg-mxl absolute flex h-full w-full rotate-y-180 flex-col rounded-lg border-[1px] p-6 backface-hidden">
+      <div className="mb-5 flex w-full justify-between">
+        <TeamBadge team={candidateName} flipped />
+        <span className="text-m font-normal">2/2</span>
+      </div>
+      <div className="flex flex-col items-center">
+        <p className="text-m mb-[30px] text-center text-2xl font-bold">
+          '기아'의 핵심공약
+        </p>
+        <div className="flex flex-col gap-1 font-normal">
+          <VotePromise promise="기숙사 개선" />
+          <VotePromise promise="교내 와이파이 개선" />
+          <VotePromise promise="전공과목 인원 확대" />
+          <VotePromise promise="교내 강의동 시설 보수" />
+          <VotePromise promise="다양한 행사 활성화" />
+        </div>
+      </div>
+    </div>
+  );
 
   return (
     <div
-      className="perspective h-100 w-[304px]"
+      className="perspective h-100 w-[304px] cursor-pointer"
       onClick={() => setFlipped(!flipped)}
     >
       <motion.div
@@ -28,42 +73,6 @@ export default function VotePromiseCard() {
     </div>
   );
 }
-
-const CardFront = () => (
-  <div className="border-m shadow-voteItem absolute flex h-full w-full flex-col rounded-lg border-[1px] bg-white p-6 backface-hidden">
-    <div className="mb-5 flex w-full justify-between">
-      <TeamBadge team="기아" />
-      <span className="text-m font-normal">1/2</span>
-    </div>
-    <div className="mb-[34px] flex w-full flex-col">
-      <VoteCandidate name="최형우" />
-    </div>
-    <div className="flex w-full flex-col">
-      <VoteCandidate name="김도영" />
-    </div>
-  </div>
-);
-
-const CardBack = () => (
-  <div className="border-m shadow-voteItem bg-mxl absolute flex h-full w-full rotate-y-180 flex-col rounded-lg border-[1px] p-6 backface-hidden">
-    <div className="mb-5 flex w-full justify-between">
-      <TeamBadge team="기아" flipped />
-      <span className="text-m font-normal">2/2</span>
-    </div>
-    <div className="flex flex-col items-center">
-      <p className="text-m mb-[30px] text-center text-2xl font-bold">
-        '기아'의 핵심공약
-      </p>
-      <div className="flex flex-col gap-1 font-normal">
-        <VotePromise promise="기숙사 개선" />
-        <VotePromise promise="교내 와이파이 개선" />
-        <VotePromise promise="전공과목 인원 확대" />
-        <VotePromise promise="교내 강의동 시설 보수" />
-        <VotePromise promise="다양한 행사 활성화" />
-      </div>
-    </div>
-  </div>
-);
 
 const TeamBadge = ({
   team,
