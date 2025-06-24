022663466a58e8d5f4e75b68bf10d66248de7722
cho4u4o <cho4u4o@gmail.com>
2025-06-24 17:45:29 +0900

    feat : add types to notice and add colors for each types

--- src/features/notice/types/notice.ts
+++ src/features/notice/types/notice.ts
@@ -1,8 +1,15 @@
-export type NoticeType = 'MESSAGE' | 'NOTIFICATION';
+export type NoticeType = 'UPCOMING' | 'NOTIFY';
+
+export type NoticeListType = NoticeType | 'ONGOING' | 'COMPLETED';
 
 export type Notice = {
   id: number;
   title: string;
-  type: NoticeType;
-  date: string;
+  startAt: string;
+  endAt: string;
+  noticeType: NoticeType;
+};
+
+export type NoticeList = Omit<Notice, 'noticeType'> & {
+  noticeStatus: NoticeListType;
 };
