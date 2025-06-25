e866641f929d00e88d8a81912ed6e32589761346
한유진 <128016678+yummjin@users.noreply.github.com>
2025-06-25 14:58:38 +0900

    fix : add id temporarily to fix CI error

--- src/screen/admin-vote-edit/ui/AdminVoteEditScreen.tsx
+++ src/screen/admin-vote-edit/ui/AdminVoteEditScreen.tsx
@@ -18,6 +18,7 @@ export default function AdminVoteEditScreen() {
             <span className="text-m">선거운동본부</span>를 선택해주세요.
           </p>
           <Card
+            id={2}
             campus="SUWON"
             title="구름 선거운동본부"
             status="예정"
@@ -26,6 +27,7 @@ export default function AdminVoteEditScreen() {
             customTo={PATH.VOTE_EDIT}
           />
           <Card
+            id={3}
             campus="SUWON"
             title="구름 선거운동본부"
             status="예정"
