79e27ad8590a8ab0e4f7d9b1aa33e51690099ade
cho4u4o <cho4u4o@gmail.com>
2025-06-26 01:05:44 +0900

    feat : add logout logic at `onLogoutClick` function

--- src/screen/home/ui/HomeScreen.tsx
+++ src/screen/home/ui/HomeScreen.tsx
@@ -5,6 +5,7 @@ import { HomeAppBar } from '@/shared/ui';
 import { HomeContainer } from '@/widgets/home/ui';
 import { useFlow } from '@/app/stackflow';
 import { PATH } from '@/shared/constants';
+import { logout } from '@/shared/utils';
 
 export default function HomeScreen() {
   const { replace } = useFlow();
@@ -14,7 +15,10 @@ export default function HomeScreen() {
       preventSwipeBack
       backgroundImage={`url(${HomeBg})`}
       appBar={HomeAppBar(
-        () => replace(PATH.LOGIN, {}),
+        () => {
+          replace(PATH.LOGIN, {}, { animate: false });
+          logout();
+        },
         () => {},
       )}
     >
