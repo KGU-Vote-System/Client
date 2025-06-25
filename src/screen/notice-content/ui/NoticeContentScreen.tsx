e87aa50a663aa00b69490fddba8d20aa97a52606
cho4u4o <cho4u4o@gmail.com>
2025-06-26 06:16:37 +0900

    refactor : reflect type change on `NoticeContentScreen`

--- src/screen/notice-content/ui/NoticeContentScreen.tsx
+++ src/screen/notice-content/ui/NoticeContentScreen.tsx
@@ -1,13 +1,13 @@
 import { AppScreen } from '@stackflow/plugin-basic-ui';
 
 import { TitleAppBar } from '@/shared/ui';
-import type { Notice } from '@/features/notice/types';
+import type { NoticeList } from '@/shared/types';
 import type { ActivityComponentType } from '@stackflow/react';
 import { NoticeContentContainer } from '@/widgets/notice-content/ui';
 
 const NoticeContentScreen: ActivityComponentType<{
-  notice: Omit<Notice, 'noticeType'>;
-}> = ({ params }: { params: { notice: Omit<Notice, 'noticeType'> } }) => {
+  notice: NoticeList;
+}> = ({ params }: { params: { notice: NoticeList } }) => {
   const { title, id, startAt, endAt } = params.notice;
   return (
     <AppScreen backgroundColor="#fff" appBar={TitleAppBar('공지사항')}>
