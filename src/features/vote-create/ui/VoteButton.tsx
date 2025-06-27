00f062e2f82c993bce4b2421edcc210acddc8c6f
cho4u4o <cho4u4o@gmail.com>
2025-06-27 13:22:37 +0900

    feat : implement complete style to vote input and button components

--- src/features/vote-create/ui/VoteButton.tsx
+++ src/features/vote-create/ui/VoteButton.tsx
@@ -1,29 +1,58 @@
+import { cn } from '@/shared/utils';
 import type { ButtonHTMLAttributes } from 'react';
-import { IoChevronDownSharp, IoChevronForwardSharp } from 'react-icons/io5';
+import { FaCheck } from 'react-icons/fa';
+import {
+  IoAdd,
+  IoChevronDownSharp,
+  IoChevronForwardSharp,
+} from 'react-icons/io5';
 
 interface VoteButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   label: string;
   arrowDown?: boolean;
+  done?: boolean;
+  className?: string;
+  plus?: boolean;
 }
 
 export default function VoteButton({
+  plus = false,
+  className,
   arrowDown = false,
   label,
   onClick,
+  done = false,
   ...rest
 }: VoteButtonProps) {
   return (
     <button
       name="mainCandidate"
-      className="border-sl flex h-17 w-full cursor-pointer items-center justify-between rounded-md border-[1px] px-5 font-normal text-[#999] focus:outline-none"
+      className={cn(
+        className,
+        done
+          ? 'border-ml text-ml shadow-voteCreateItem'
+          : 'border-sl text-[#999]',
+        'relative flex h-17 w-full cursor-pointer items-center justify-between rounded-md border-[1px] px-5 font-normal focus:outline-none',
+      )}
       onClick={onClick}
       {...rest}
     >
       {label}
-      {arrowDown ? (
-        <IoChevronDownSharp size={20} className="text-sl" />
+
+      {done ? (
+        <div className="bg-ml absolute top-5.5 right-5 mb-1.5 grid size-6 place-items-center rounded-full transition duration-150">
+          <FaCheck className="text-white" size={14} />
+        </div>
       ) : (
-        <IoChevronForwardSharp size={20} className="text-sl" />
+        <>
+          {plus ? (
+            <IoAdd size={20} className="text-sl" />
+          ) : arrowDown ? (
+            <IoChevronDownSharp size={20} className="text-sl" />
+          ) : (
+            <IoChevronForwardSharp size={20} className="text-sl" />
+          )}
+        </>
       )}
     </button>
   );
