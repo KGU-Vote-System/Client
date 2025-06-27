d7a87f0d823a6472fcf8bf89e90cc77b423c3f8d
cho4u4o <cho4u4o@gmail.com>
2025-06-27 13:27:07 +0900

    feat : implement animation to `VoteCreateCompleteScreen`

--- src/screen/vote-create-complete/ui/VoteCreateCompleteScreen.tsx
+++ src/screen/vote-create-complete/ui/VoteCreateCompleteScreen.tsx
@@ -4,7 +4,7 @@ import { useFlow } from '@/app/stackflow';
 import { Button, NoBackAppBar } from '@/shared/ui';
 import { VoteBg } from '@/assets/image';
 import { CreateCompleteIcon } from '@/assets/icon';
-
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 export default function VoteCreateCompleteScreen() {
   const { pop } = useFlow();
 
@@ -22,6 +22,15 @@ export default function VoteCreateCompleteScreen() {
           <p className="text-s mb-12 text-center text-xl font-semibold">
             투표는 대시보드에서 수정 가능해요
           </p>
+          <div className="absolute top-70 left-1/2 -translate-x-1/2">
+            <DotLottieReact
+              style={{ width: 300, height: 300 }}
+              src="https://lottie.host/a0efbf08-977e-49af-926d-01b31d530dd8/7e8jbdwS7b.lottie"
+              autoplay
+              className="scale-500"
+            />
+          </div>
+
           <img src={CreateCompleteIcon} alt="voteComplete" className="mb-20" />
           <Button intent="gradient" onClick={() => pop({ animate: false })}>
             홈으로 돌아가기
